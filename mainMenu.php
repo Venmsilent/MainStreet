2f004dfc6f20602b39ab56a197619190bce400f7
molliepath <molliepath6251@gmail.com>
2026-04-15 18:12:44 -0400

    header changes

---
+++ mainMenu.php
@@ -0,0 +1,34 @@
+<div class="w3-container headerBox">
+    <div class="w3-dropdown-hover">
+        <button class="w3-button w3-black">Customer</button>
+        <div class="w3-dropdown-content w3-bar-block w3-border">
+            <a href="newCustomer.php" class="w3-bar-item w3-button">Add</a>
+            <a href="showCustomers.php" class="w3-bar-item w3-button">View All</a>
+            <a href="deleteCustomer.php" class="w3-bar-item w3-button">Delete</a>
+        </div>
+    </div>
+
+    <div class="w3-dropdown-hover">
+        <button class="w3-button w3-black">Orders</button>
+        <div class="w3-dropdown-content w3-bar-block w3-border">
+            <a href="newOrder.php" class="w3-bar-item w3-button">Add</a>
+            <a href="showOrders.php" class="w3-bar-item w3-button">View All</a>
+        </div>
+    </div>
+
+    <div class="w3-dropdown-hover">
+        <button class="w3-button w3-black">Products</button>
+        <div class="w3-dropdown-content w3-bar-block w3-border">
+            <a href="newProduct.php" class="w3-bar-item w3-button">Add</a>
+            <a href="showProducts.php" class="w3-bar-item w3-button">View All</a>
+            <a href="deleteProduct.php" class="w3-bar-item w3-button">Delete</a>
+        </div>
+    </div>
+
+    <div class="w3-dropdown-hover">
+        <button class="w3-button w3-black">Info</button>
+        <div class="w3-dropdown-content w3-bar-block w3-border">
+            <a href="about.php" class="w3-bar-item w3-button">About</a>
+        </div>
+    </div>
+</div>
