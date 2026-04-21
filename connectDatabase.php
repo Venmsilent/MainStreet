93ceae66ed9a307ad7e1e80ae8f210fbd7f27076
molliepath <molliepath6251@gmail.com>
2026-04-21 19:15:11 -0400

    fixed connectDatabase

--- connectDatabase.php
+++ connectDatabase.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username = "guest";
+$password = "ggcITEC4450@";
 $dbname = "mainstreetDB";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
